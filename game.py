c8c3dacaadb4ede297536e14d56bb408988f2b14
RedMythic1 <ab7552@pleasantonusd.net>
2023-02-20 07:58:05 -0800

    Co-authored-by: Veerrohit Veeravadivel <The-Coding-Kid@users.noreply.github.com>

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from random import choice, randint
 openai.api_key = "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"
 
 import arcade
+import time
 
 init_coords = [1,5]
 init_tree = [1,75]
@@ -14,6 +15,14 @@ incr_tree = 0
 
 path = "assets//foliage"
 dir_list = os.listdir(path)
+path2 = "assets//people"
+dir_list2 = os.listdir(path2)
+
+charecters = []
+
+wealth = ["poor", "rich", "normal"]
+first_names = ["John", "Daniel", "Martha", "Greta", "Felix", "Jimmy", "Mark", "Mohammed", "Hajjar", "Arush", "Eli", "Sid", "Harkaran", "Dhairya", "Dwayne", "Jack", "Harry", "Abdullah", "Rizawadh"]
+last_names = ["Bhatia", "Ahmed", "Blackstone", "Smith", "Johnson", "Kjelberg", "Thunberg", "Singh", "Vienna", "London", "Cheng"]
 
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
@@ -62,19 +71,44 @@ class MyGame(arcade.Window):
         
         # Initialize Scene
         self.scene = arcade.Scene()
+        
+        layer_options = {
+            LAYER_NAME_PLATFORMS: {
+                "use_spatial_hash": True,
+            },
+            LAYER_NAME_COINS: {
+                "use_spatial_hash": True,
+            },
+            LAYER_NAME_DONT_TOUCH: {
+                "use_spatial_hash": True,
+            },
+        }
+
 
         # Create the Sprite lists
-        self.scene.add_sprite_list("Person")
+        self.scene.add_sprite_list("People", LAYER_NAME_BACKGROUND)
         self.scene.add_sprite_list("Buildings")
         self.scene.add_sprite_list("Grass", use_spatial_hash=True)
         self.scene.add_sprite_list("Foliage", use_spatial_hash=False)
-
+        y=0
         # Set up the player, specifically placing it at these coordinates.
-        image_source = "assets/people/tan-green.gif"
+        image_source = f"assets/people/white-blue2.png"
         self.person_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        self.person_sprite.center_x = 64
-        self.person_sprite.center_y = 10
-        self.scene.add_sprite("Player", self.person_sprite)
+        self.person_sprite.center_x = round(self.width/randint(1,15),0)
+        self.person_sprite.center_y = 20
+        coords = f"{self.person_sprite.center_x} 10"
+        current_guy = f"{choice(first_names)} {choice(last_names)}"
+        players = {
+                        "name": f'{current_guy}',
+                        'wealth': choice(wealth),
+                        'shops': randint(1,10),
+                        'lives': randint(1,10),
+                        'coordinates': [self.person_sprite.center_x, self.person_sprite.center_y],
+                        'file': image_source
+                    }
+        charecters.append(players)
+        self.scene.add_sprite("People", self.person_sprite)
+
         while incr_building <=100000:
             buildings = arcade.Sprite(
                     f"assets/buildings/Building{randint(1,3)}.png", .3
@@ -129,7 +163,7 @@ class MyGame(arcade.Window):
         self.gui_camera.use()
 
         # Draw our score on the screen, scrolling it with the viewport
-        score_text = f"Score: {self.score}"
+        score_text = f"Day {self.score}"
         arcade.draw_text(
             score_text,
             10,
@@ -153,18 +187,6 @@ class MyGame(arcade.Window):
         player_centered = screen_center_x, screen_center_y
 
         self.camera.move_to(player_centered)
-    def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed."""
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.score +=1
-            if self.physics_engine.can_jump():
-                self.person_sprite.change_y = PLAYER_JUMP_SPEED
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.person_sprite.change_y = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.person_sprite.change_x = -PLAYER_MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.person_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key."""
@@ -180,10 +202,30 @@ class MyGame(arcade.Window):
 
     def on_update(self, delta_time):
         """Movement and game logic"""
-
+        time.sleep(.05)
+        image_source = charecters[0]['file']
+        print(image_source)
+        self.person_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        print(charecters[0]['coordinates'])
+        self.person_sprite.center_x = charecters[0]['coordinates'][0]
+        self.person_sprite.center_y = charecters[0]['coordinates'][1]
+        print(self.person_sprite.center_x)
+        newint = randint(-40,40)  
+        if charecters[0]['coordinates'][0]>=40:
+            self.person_sprite.change_x = newint
+        else:
+            self.person_sprite.change_x += 80
+        charecters[0]['coordinates'][0] = self.person_sprite.change_x+self.person_sprite.center_x
+        charecters[0]['coordinates'][1] = self.person_sprite.change_y+self.person_sprite.center_y
+        self.person_sprite.position = [charecters[0]['coordinates'][0], charecters[0]['coordinates'][1]]
+        self.scene.get_sprite_list("People").clear()
+        self.scene.add_sprite("People", self.person_sprite)
+        self.score = round(self.score+1/10,0)
         # Move the player with the physics engine
         self.physics_engine.update()
+        self.person_sprite.update_animation()
         self.center_camera_to_player()
+        time.sleep(.05)
 
 
 def main():
